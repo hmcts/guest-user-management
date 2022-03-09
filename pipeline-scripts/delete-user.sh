c7b09c366ff96f0f6205c90171f967f9b4c3d9fa
msl8r <matt.slater@hmcts.net>
2022-03-09 10:00:46 +0000

    DTSPO-6998 - tidy

--- pipeline-scripts/delete-user.sh
+++ pipeline-scripts/delete-user.sh
@@ -4,7 +4,7 @@ set -e
 branch=$1
 
 delete_user() {
-  echo "Branch is $branch"
+
   if [[ $branch == "master" ]]; then
     echo "Deleting user ${3} with the mail address of ${2} and object ID of ${1}"
     # az ad user delete --id
