a9cb24da7d3a6e28898277bb4417d1e5078eda8e
msl8r <matt.slater@hmcts.net>
2022-03-09 10:34:15 +0000

    DTSPO-6998 - tidy

--- pipeline-scripts/delete-user.sh
+++ pipeline-scripts/delete-user.sh
@@ -7,6 +7,7 @@ delete_user() {
 
   if [[ $branch == "master" ]]; then
     echo "Deleting user ${3} with the mail address of ${2} and object ID of ${1}"
+    # az ad user update --id ${1} --account-enabled false
     # az ad user delete --id
   else
     echo "Plan: user ${3}, mail address ${2}, object ID ${1}"
