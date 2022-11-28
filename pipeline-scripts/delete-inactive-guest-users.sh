e8e6be7b15bb13c90cdfb22c2da2c74eb7637c4a
Matt Slater <72858053+msl8r@users.noreply.github.com>
2022-11-28 12:51:41 +0000

    update jq command now that guest file is slightly different

--- pipeline-scripts/delete-inactive-guest-users.sh
+++ pipeline-scripts/delete-inactive-guest-users.sh
@@ -38,7 +38,7 @@ delete_inactive_guests() {
   jq -s 'map(.value[])' users-?.json > ${users_file}
   
 
-  inactive_users_count=$(jq -r '.value[] | select(.signInActivity.lastSignInDateTime < "'${max_inactive_date}'" and .signInActivity.lastNonInteractiveSignInDateTime < "'${max_inactive_date}'") | .id' ${users_file} | wc -l)
+  inactive_users_count=$(jq -r '. | select(.signInActivity.lastSignInDateTime < "'${max_inactive_date}'" and .signInActivity.lastNonInteractiveSignInDateTime < "'${max_inactive_date}'") | .id' ${users_file} | wc -l)
   
   
   if [[ ${inactive_users_count} -gt 0 ]]; then
