b488b5162c5f3015400f1dd40cb45edfe8b81e42
Matt Slater <72858053+msl8r@users.noreply.github.com>
2022-11-29 12:29:04 +0000

    fix pagination issues (#9)
    * fix pagination issues
    
    * fix command now guest file is slightly different
    
    * update jq command now that guest file is slightly different
    
    * revert filter to what it was
    
    * fix az cli command
    
    * Update delete-inactive-guest-users.sh
    
    * Update delete-inactive-guest-users.sh
    
    * Ammend the az rest command to try and get it working
    
    * Ammend the az rest command to try and get it working
    
    * Fix az command to get inactive guest users
    
    * Fix jq command
    
    * Fix jq command
    
    * Fix jq command
    
    * Update delete-inactive-guest-users.sh

--- pipeline-scripts/delete-inactive-guest-users.sh
+++ pipeline-scripts/delete-inactive-guest-users.sh
@@ -9,6 +9,9 @@ min_user_age_days=7
 max_inactive_date=$(date +%Y-%m-%dT%H:%m:%SZ -d "${max_inactive_days} days ago")
 min_user_age_date=$(date +%Y-%m-%dT%H:%m:%SZ -d "${min_user_age_days} days ago")
 
+echo "max_inactive_date=${max_inactive_date}"
+echo "min_user_age_date=${min_user_age_date}"
+
 users_file=guests.json
 
 . pipeline-scripts/delete-user.sh $branch
@@ -17,9 +20,27 @@ users_file=guests.json
 delete_inactive_guests() {
 
   # Create file with list of guest users that have accepted their invite
-  az rest --method GET --uri "https://graph.microsoft.com/beta/users?\$top=999&\$filter=externalUserState eq 'Accepted' and userType eq 'Guest' and createdDateTime le ${min_user_age_date}&\$select=id,displayName,signInActivity,createdDateTime,mail" > ${users_file}
+  NEXT_LINK=
+  counter=0
+
+  until [[ "${NEXT_LINK}" == "null" ]]; do
+
+    if [[ ${counter} == 0 ]]; then
+      az rest --method get --uri "https://graph.microsoft.com/beta/users?top=999&filter=externalUserState eq 'Accepted' and createdDateTime le ${min_user_age_date}&select=id,displayName,signInActivity,createdDateTime,mail" > users-${counter}.json
+    else
+      az rest --method get --uri "${NEXT_LINK}" > users-${counter}.json
+    fi
+
+    NEXT_LINK=$(jq -r '."@odata.nextLink"' users-${counter}.json)
+
+    counter=$(( counter + 1 ))
+
+  done
+
+  jq -s 'map(.value[])' users-?.json > ${users_file}
   
-  inactive_users_count=$(jq -r '.value[] | select(.signInActivity.lastSignInDateTime < "'${max_inactive_date}'" and .signInActivity.lastNonInteractiveSignInDateTime < "'${max_inactive_date}'") | .id' ${users_file} | wc -l)
+
+  inactive_users_count=$(jq -r '.[] | select(.signInActivity.lastSignInDateTime < "'${max_inactive_date}'" and .signInActivity.lastNonInteractiveSignInDateTime < "'${max_inactive_date}'") | .id' ${users_file} | wc -l)
   
   
   if [[ ${inactive_users_count} -gt 0 ]]; then
@@ -29,7 +50,7 @@ delete_inactive_guests() {
     do
       delete_user "$object_id" "$mail" "$display_name" &
 
-    done <<< "$(jq -r '.value[] | select(.signInActivity.lastSignInDateTime < "'${max_inactive_date}'" and .signInActivity.lastNonInteractiveSignInDateTime < "'${max_inactive_date}'") | "\(.id) \(.mail) \(.displayName)"' ${users_file})"
+    done <<< "$(jq -r '.[] | select(.signInActivity.lastSignInDateTime < "'${max_inactive_date}'" and .signInActivity.lastNonInteractiveSignInDateTime < "'${max_inactive_date}'") | "\(.id) \(.mail) \(.displayName)"' ${users_file})"
     wait
   else
     echo "No inactive users found, nothing to do"
