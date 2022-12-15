cefaaa6d7c0a8bddd1933666be39a9afc8fdcc9d
matt.slater <matt.slater@justice.gov.uk>
2022-12-15 18:57:35 +0000

    Update delete users script

--- pipeline-scripts/delete-inactive-guest-users.sh
+++ pipeline-scripts/delete-inactive-guest-users.sh
@@ -4,10 +4,10 @@ set -e
 branch=$1
 
 # Number of days before deletion date that a user will start getting notified about being deleted
-warn_inactive_days=2
+warn_inactive_days=7
 
 # Number of days a user can be inactive before being deleted
-delete_inactive_days=5
+delete_inactive_days=31
 
 min_user_age_days=7
 max_inactive_days=$((${warn_inactive_days} + ${delete_inactive_days}))
