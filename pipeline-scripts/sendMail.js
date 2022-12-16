b059f854140e4b12a0d4e597ad55476395ebc450
matt.slater <matt.slater@justice.gov.uk>
2022-12-16 10:05:06 +0000

    pass api key via argument

--- pipeline-scripts/sendMail.js
+++ pipeline-scripts/sendMail.js
@@ -1,15 +1,14 @@
 const commandArgs = process.argv.slice(2)
+const emailAddress = commandArgs[0]
+const name = commandArgs[1]
+const apiKey = commandArgs[2]
 
 const NotifyClient = require('notifications-node-client').NotifyClient;
-
 const notifyClient = new NotifyClient(apiKey);
 
 const templateId = "b0c88c5b-76c3-4c17-9db4-63017e0f9a21"
 const azurePortalURL = "https://portal.azure.com/hmcts.net"
 
-const emailAddress = commandArgs[0]
-const name = commandArgs[1]
-const apiKey = commandArgs[2]
 let personalisation = {
         'name': name,
         'url': azurePortalURL,
