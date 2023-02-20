e01f8e0a62c229d5338b406804d9196fda137c68
Matt Slater <72858053+msl8r@users.noreply.github.com>
2023-02-20 11:40:32 +0000

    Enable deleting unaccepted guest invites (#20)
    * Enable deleting unaccepted guest invites
    
    * Update pipeline-scripts/delete-user.sh
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- pipeline-scripts/delete-user.sh
+++ pipeline-scripts/delete-user.sh
@@ -6,11 +6,10 @@ branch=$1
 delete_user() {
 
   if [[ $branch == "master" ]]; then
-    echo "Not deleting"
-     # echo "Deleting user ${3} with the mail address of ${2} and object ID of ${1}"
+      echo "Deleting user ${3} with the mail address of ${2} and object ID of ${1}"
      # TODO https://github.com/Azure/azure-cli/issues/12946#issuecomment-737196942
      # az ad user delete --id ${1}
-     #az rest --method DELETE --uri "https://graph.microsoft.com/v1.0/users/${1}"
+     az rest --method DELETE --uri "https://graph.microsoft.com/v1.0/users/${1}"
   else
     echo "Plan: user ${3}, mail address ${2}, object ID ${1}"
   fi
